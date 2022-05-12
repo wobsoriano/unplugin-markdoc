51cfcfc6f423a995bfdb0599f526edc11fbcebb2
Robert Soriano <sorianorobertc@gmail.com>
2022-05-12 13:34:38 -0700

    add description

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export const foo = 'foo'
+import type { Plugin } from 'vite'
+import { dataToEsm } from '@rollup/pluginutils'
+import Markdoc from '@markdoc/markdoc'
+
+const mdExtRE = /\.(md)$/i
+
+export default function plugin(): Plugin {
+  return {
+    name: 'vite-plugin-markdoc',
+    enforce: 'pre',
+    transform(code, id) {
+      if (!mdExtRE.test(id))
+        return null
+
+      const ast = Markdoc.parse(code)
+      const content = Markdoc.transform(ast)
+
+      return {
+        code: dataToEsm(content, {
+          preferConst: true,
+          namedExports: true,
+        }),
+        map: { mappings: '' },
+      }
+    },
+  }
+}
