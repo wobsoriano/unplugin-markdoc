6a3f74f657790962d338af4c71962570892d1357
wobsoriano <sorianorobertc@gmail.com>
2022-11-22 23:33:36 -0800

    feat: add support for rollup,webpack,esbuild and nuxt

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import type { Plugin } from 'vite'
+import { createUnplugin } from 'unplugin'
 import { dataToEsm } from '@rollup/pluginutils'
 import Markdoc from '@markdoc/markdoc'
-
-export type Options = Parameters<typeof Markdoc.transform>['1']
+import type { Options } from './types'
 
 const mdExtRE = /\.(md)$/i
 
@@ -13,23 +12,21 @@ export function transformMarkdown(code: string, options?: Options) {
   return content
 }
 
-export default function (options?: Options): Plugin {
-  return {
-    name: 'vite-plugin-markdoc',
-    enforce: 'pre',
-    transform(code, id) {
-      if (!mdExtRE.test(id))
-        return null
+export default createUnplugin<Options | undefined>(options => ({
+  name: 'unplugin-markdoc',
+  enforce: 'pre',
+  transform(code, id) {
+    if (!mdExtRE.test(id))
+      return null
 
-      const esm = transformMarkdown(code, options)
+    const esm = transformMarkdown(code, options)
 
-      return {
-        code: dataToEsm(esm, {
-          preferConst: true,
-          namedExports: true,
-        }),
-        map: { mappings: '' },
-      }
-    },
-  }
-}
+    return {
+      code: dataToEsm(esm, {
+        preferConst: true,
+        namedExports: true,
+      }),
+      map: { mappings: '' },
+    }
+  },
+}))
