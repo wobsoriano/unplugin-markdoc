50300323d3c27f8f10402eacaeb12e426465fb24
wobsoriano <sorianorobertc@gmail.com>
2023-01-06 09:34:53 -0800

    update playground

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import Markdoc from '@markdoc/markdoc'
 import html from './contents/doc.md?html'
-
-// const html = Markdoc.renderers.html(content)
 </script>
 
 <template>
