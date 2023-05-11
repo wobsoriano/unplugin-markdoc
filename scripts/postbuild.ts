8947a7012f6aa4faa7eee3b78a59822f28ff5c7d
wobsoriano <sorianorobertc@gmail.com>
2023-05-10 21:36:37 -0700

    update imports

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,5 +1,5 @@
 import { basename, dirname, resolve } from 'node:path'
-import { promises as fs } from 'node:fs'
+import { readFile, writeFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 import fg from 'fast-glob'
 import chalk from 'chalk'
@@ -13,10 +13,10 @@ async function run() {
   })
   for (const file of files) {
     console.log(chalk.cyan.inverse(' POST '), `Fix ${basename(file)}`)
-    let code = await fs.readFile(file, 'utf8')
+    let code = await readFile(file, 'utf8')
     code = code.replace('exports.default =', 'module.exports =')
     code += 'exports.default = module.exports;'
-    await fs.writeFile(file, code)
+    await writeFile(file, code)
   }
 }
 
